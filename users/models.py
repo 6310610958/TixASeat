032cc1a8fd34120711a0b05bc6c1989d724795d2
Variya Kittiwattanachok <variyak7@gmail.com>
2023-02-11 00:12:34 +0700

    Check

---
+++ users/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+# Create your models here.
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    # profile_pic= models.ImageField(upload_to='profile_pic/CustomerProfilePic/',null=True,blank=True)
+    # address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name
+
+
+class Product(models.Model):
+    CATEGORY =(
+        ('Concert', 'Concert'),
+        ('Musical', 'Musical'),
+        ('Sport', 'Sport'),
+        ('Other', 'Other')
+    )
+    STATUS=(
+        ('Available', 'Available'),
+        ('Sold', 'Sold')
+    )
+    # user=models.OneToOneField(User,on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
+    name=models.CharField(max_length=40)
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    price = models.PositiveIntegerField()
+    description=models.CharField(max_length=40)
+    seat=models.CharField(max_length=20, null=True)
+    location=models.CharField(max_length=64, null=True)
+    date=models.CharField(max_length=40, null=True)
+    time=models.CharField(max_length=12, null=True)
+    category=models.CharField(max_length=50,null=True,choices=CATEGORY)
+    status=models.CharField(max_length=50,null=True,choices=STATUS, default='Available')
+    def __str__(self):
+        return f'{self.name} Seat: {self.seat} Date: {self.date} Category: {self.category} Status: {self.status}'
+
+
+class Orders(models.Model):
+    STATUS =(
+        ('Pending','Pending'),
+        ('Successful','Successful'),
+    )
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
+    email = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=500,null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    order_date= models.DateField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=50,null=True,choices=STATUS)
+
+
+class Feedback(models.Model):
+    name=models.CharField(max_length=40)
+    feedback=models.CharField(max_length=500)
+    date= models.DateField(auto_now_add=True,null=True)
+    def __str__(self):
+        return self.name
+
+class Payment(models.Model):
+    BANK=(
+        ('Kasikorn Bank', 'Kasikorn Bank'),
+        ('Bangkok Bank', 'Bangkok Bank'),
+        ('Krungthai Bank', 'Krungthai Bank'),
+        ('Krungsri Bank', 'Krungsri Bank'),
+        ('TTB Bank', 'TTB Bank'),
+        ('SCB Bank', 'SCB Bank'),
+        ('Other Bank form list', 'Other Bank form list'),
+    )
+    user=models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
+    date=models.CharField(max_length=40, null=True)
+    time=models.CharField(max_length=12, null=True)
+    amount=models.PositiveIntegerField()
+    paymentslip=models.ImageField(upload_to='paymentslip/',null=True,blank=True)
+    last4=models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
+    bank=models.CharField(max_length=50,null=True,choices=BANK)
+
+    def __str__(self):
+        return f'{self.user} Amount: {self.amount} Date: {self.date} Time: {self.time} Bank: {self.bank}'
